12d472cd2b5f6ba196a2a7ef8c85ad96aec0fc57
Ved0303 <ved2005@gmail.com>
2023-04-26 18:11:06 +0530

    add class Manpower

---
+++ src/main/java/com/kheti/Inventory/model/ManPower.java
@@ -0,0 +1,119 @@
+package com.kheti.Inventory.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class ManPower {
+	
+	 @Id
+	 @GeneratedValue(strategy=GenerationType.AUTO)
+	int id;
+	String name;
+	String email;
+	String phone;
+	String aadhar;
+	String pan;
+	String type; //Employee/ Contractor/ Individual
+
+
+	public ManPower() {}
+
+
+	public ManPower(int id, String name, String email, String phone, String aadhar, String pan, String type) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.aadhar = aadhar;
+		this.pan = pan;
+		this.type = type;
+	}
+	
+	public ManPower( String name, String email, String phone, String aadhar, String pan, String type) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.aadhar = aadhar;
+		this.pan = pan;
+		this.type = type;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getPhone() {
+		return phone;
+	}
+
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+
+	public String getAadhar() {
+		return aadhar;
+	}
+
+
+	public void setAadhar(String aadhar) {
+		this.aadhar = aadhar;
+	}
+
+
+	public String getPan() {
+		return pan;
+	}
+
+
+	public void setPan(String pan) {
+		this.pan = pan;
+	}
+
+
+	public String getType() {
+		return type;
+	}
+
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+
+	
+
+}
